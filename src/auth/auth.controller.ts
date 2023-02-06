1ec6daf553c726bdd2e20141d771d417f8caac52
laxlax <macbookprom2@MacBook-Pro-de-MACBOOK.local>
2023-02-06 12:59:18 -0300

    create passport local authentication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Post, UseGuards } from '@nestjs/common';
+import { Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth.service';
 
 @Controller('api/auth')
 export class AuthController {
+    constructor(private readonly authService: AuthService) { }
 
     @UseGuards(AuthGuard('local'))
     @Post('login')
-    async login() {
-        return { success: true };
+    async login(@Req() req: any) {
+        return await this.authService.login(req.user);
     }
 }
--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -11,12 +11,9 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
     }
 
     async validate(email: string, password: string) {
-        console.log(email, password)
         const user = await this.authService.validateUser(email, password);
-        console.log(user);
         if (!user)
             throw new UnauthorizedException(MessagesUtils.PASSWORD_OR_EMAIL_INVALID)
-
         return user;
     }
 }
