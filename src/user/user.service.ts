92ab9beecce88c9a57104fba984c9a596d0f03c8
laxlax <macbookprom2@MacBook-Pro-de-MACBOOK.local>
2023-02-14 10:52:04 -0300

    modify import path to file userOptions

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,7 +4,7 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
-import { UserOptions } from './types/UserOptions';
+import { UserOptions } from 'src/user/types/userOptions';
 
 @Injectable()
 export class UserService {
